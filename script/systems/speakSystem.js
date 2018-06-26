5ec121f1c467b0e486f3d38094de95bd08e4b578
Strobostream <40342605+Strobostream@users.noreply.github.com>
2018-06-26 20:53:17 +0200

    Create speakSystem.js

---
+++ script/systems/speakSystem.js
@@ -0,0 +1,33 @@
+//ROUGH TTS SYSTEM PHANTOMBOT
+var TTS = {
+    speak: function(inputText){//Define function getting & playing TTS
+	var speechText = "<voice>" + inputText;
+	//Post speech text.
+	$.say(speechText);
+	$.panelsocketserver.alertImage(speechText);
+    }
+};
+//Adding text to speech to PhantomBot
+(function(){
+    //Define cost of shouting:
+    var PRICE = 10;
+    //Define function calling tts.
+    function speak(textToSpeak){
+	TTS.speak(textToSpeak);
+    }
+    //Bind tts to a command.
+    $.bind('command',function(event){
+	var command = event.getCommand(),
+	    inputText = event.getArgs()[0];
+	//Check command.
+	if (command.equalsIgnoreCase('shout')){
+	    speak(inputText);
+	}
+    });
+	//Register command.
+	$.bind('initReady',function(){
+	    if($.bot.isModuleEnabled('./systems/speakSystem.js')){
+		$.registerChatCommand('./systems/speakSystem.js','shout',PRICE);
+	    }
+	});
+})();
